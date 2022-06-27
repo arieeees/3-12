9a951174401b9ced70d5b6f0cd35e5d997595cf8
arieeees <105973830+arieeees@users.noreply.github.com>
2022-06-27 10:46:46 +0000

    [Add]3-12

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'index/new'
+  post 'index' => 'index#create'
   get 'index/show'
   get 'index/edit'
   get 'index/destroy'
-  get '/top' => 'homes#top'
+  get 'top' => 'homes#top'  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
